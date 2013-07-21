a24fa498e9451c2e92d5aecd2e96a390f3502cab
fenderglass <fenderglass@gmail.com>
2013-07-21 13:29:47 +0600

    tempfiles instead pipes

--- scripts/ext_tools.py
+++ scripts/ext_tools.py
@@ -1,7 +1,7 @@
 import logging
 import subprocess
+import tempfile
 import fasta_reader as fr
-from cStringIO import StringIO
 from collections import namedtuple
 import re
 
@@ -19,29 +19,39 @@ AlignInfo = namedtuple("AlignInfo", ["begin", "end", "score"])
 def graph_clust(fasta_dict, kmer, missmatches):
     logger.debug(GRAPH_CLUST_EXEC + " for {0} seqs started".format(len(fasta_dict)))
 
-    buffer = StringIO()
-    fr.write_fasta(fasta_dict, buffer)
+    in_file = tempfile.SpooledTemporaryFile()
+    out_file = tempfile.SpooledTemporaryFile()
+    fr.write_fasta(fasta_dict, in_file)
+    in_file.flush()
+    in_file.seek(0)
 
     cmdline = [GRAPH_CLUST_EXEC, "-k", str(kmer), "-m", str(missmatches), "-q"]
-    process = subprocess.Popen(cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    child_stdout, _ = process.communicate(input=buffer.getvalue())
+    process = subprocess.Popen(cmdline, stdin=in_file, stdout=out_file)
+    process.wait()
+    out_file.flush()
+    out_file.seek(0)
 
     logger.debug(GRAPH_CLUST_EXEC + " finished")
-    return fr.read_cluster(StringIO(child_stdout))
+    return fr.read_cluster(out_file)
 
 
 def hierarchial_clust(fasta_dict, cutoff):
     logger.debug(HIERARCH_CLUST_EXEC + " for {0} seqs started".format(len(fasta_dict)))
 
-    buffer = StringIO()
-    fr.write_fasta(fasta_dict, buffer)
+    in_file = tempfile.SpooledTemporaryFile()
+    out_file = tempfile.SpooledTemporaryFile()
+    fr.write_fasta(fasta_dict, in_file)
+    in_file.flush()
+    in_file.seek(0)
 
     cmdline = [HIERARCH_CLUST_EXEC, "-c", str(cutoff), "-q"]
-    proc = subprocess.Popen(cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    child_stdout, _ = proc.communicate(input=buffer.getvalue())
+    proc = subprocess.Popen(cmdline, stdin=in_file, stdout=out_file)
+    proc.wait()
+    out_file.flush()
+    out_file.seek(0)
 
     logger.debug(HIERARCH_CLUST_EXEC + " finished")
-    return fr.read_cluster(StringIO(child_stdout))
+    return fr.read_cluster(out_file)
 
 
 def xalign(fasta_dict, query, match, missmatch, indel):
@@ -50,17 +60,22 @@ def xalign(fasta_dict, query, match, missmatch, indel):
     trhld = qry_len * match - (match + indel)                   #one indel is allowed
     #assert qry_len == 9
 
-    buffer = StringIO()
-    fr.write_fasta(fasta_dict, buffer)
+    in_file = tempfile.SpooledTemporaryFile()
+    out_file = tempfile.SpooledTemporaryFile()
+    fr.write_fasta(fasta_dict, in_file)
+    in_file.flush()
+    in_file.seek(0)
 
     logger.debug("Calling xalign with querry \"{0}\"".format(query))
     cmdline = [XALIGN_EXEC, "-t", str(trhld), "-q", query, "-m", str(match),
                     "-x", str(missmatch), "-i", str(indel), "-d", str(indel)]
-    child = subprocess.Popen(cmdline, stdin = subprocess.PIPE, stdout = subprocess.PIPE)
-    child_stdout, _ = child.communicate(input=buffer.getvalue())
+    child = subprocess.Popen(cmdline, stdin=in_file, stdout=out_file)
+    child.wait()
+    out_file.flush()
+    out_file.seek(0)
 
     out_dict = {}
-    for line in child_stdout.splitlines():
+    for line in out_file:
         values = line.strip().split(" ")
         header = values[0][1:]
         aligns = values[1:]
@@ -76,13 +91,18 @@ def xalign(fasta_dict, query, match, missmatch, indel):
 def muscle(fasta_dict):
     logger.debug("Running muscle for {0} seqs".format(len(fasta_dict)))
 
-    buffer = StringIO()
-    fr.write_fasta(fasta_dict, buffer)
+    in_file = tempfile.SpooledTemporaryFile()
+    out_file = tempfile.SpooledTemporaryFile()
+    fr.write_fasta(fasta_dict, in_file)
+    in_file.flush()
+    in_file.seek(0)
 
     cmdline = [MUSCLE_EXEC, "-diags", "-maxiters", "2", "-quiet"]
-    proc = subprocess.Popen(cmdline, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    child_stdout, _ = proc.communicate(input=buffer.getvalue())
+    proc = subprocess.Popen(cmdline, stdin=in_file, stdout=out_file)
+    proc.wait()
+    out_file.flush()
+    out_file.seek(0)
 
     logger.debug("Muscle finished")
-    return fr.read_fasta(StringIO(child_stdout))
+    return fr.read_fasta(out_file)
 
